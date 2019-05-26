4bbe947931c7ed4994ab9c491cd7b6a930278fe7
ell10t <samarcev.ilya@mail.ru>
2019-05-26 12:06:44 +0300

    director

--- director.php
+++ director.php
@@ -13,8 +13,29 @@ if($_SESSION['role'] != 'Директор' || empty($_SESSION['role']))
     <meta charset="UTF-8">
     <title> РУКОВОДИТЕЛЬ </title>
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/manager.css">
+    <link rel="stylesheet" href="css/forms.css">
 </head>
 <body>
     <a href="logout.php"> ВЫЙТИ </a>
+
+    <h2 id="title"> ИЗДЕЛИЯ: </h2>
+    <form id="products" action="" method="POST">
+        <?
+            $query = "SELECT * FROM products";
+            $result = mysqli_query($link, $query);
+
+            while ($cloth_info = mysqli_fetch_row($result))
+            {
+                echo "<div id='info'>";
+                    echo "<img src='$cloth_info[4]'>";
+                    echo "<p> Имя: $cloth_info[1] </p>"; 
+                    echo "<p> Ширина: $cloth_info[2] </p>";
+                    echo "<p> Длина: $cloth_info[3] </p>";
+                    echo "<p> Краткая информация: $cloth_info[5] </p>"; 
+                echo "</div>";
+            }
+        ?>
+    </form>
 </body>
 </html>
